5985f4cdf48bb1f268f86f6efd511fa8657b76ad
etfsurfer <etfsurfer@gmail.com>
2026-02-16 11:06:14 +0000

    Update main.js and recipe.html

--- main.js
+++ main.js
@@ -129,7 +129,7 @@ window.disqus_config = function () {
 document.addEventListener('DOMContentLoaded', function() {
     (function() { // DON'T EDIT BELOW THIS LINE
         var d = document, s = d.createElement('script');
-        s.src = 'https://surfer22-1.disqus.com/embed.js';
+        s.src = 'https://surfer-1.disqus.com/embed.js';
         s.setAttribute('data-timestamp', +new Date());
         (d.head || d.body).appendChild(s);
     })();
